a9e7dc0e34e8b59a9eaee9ebfd66c6c728ca8f35
Tito STR Macbook pro <humberto.trujillo@uabc.edu.mx>
2021-02-11 19:08:26 -0800

    Ejemplo de problema

---
+++ diseno_de_interacciones-2021-1/Assets/CodeProblems/Problem1.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Problem1 : MonoBehaviour
+{
+    void Start()
+    {
+        int[] nums = {8,1,2,2,3};
+        int[] output = NumbersLessThan(nums);
+        foreach (var num in output)
+        {
+            Debug.Log(num);
+        }
+    }
+
+    //Investigar complejidad de tiempo - orden del algoritmo - tiempo de ejecución
+    // time complexity
+    // O(n) -> lineal
+    // O(n^2) -> cuadratica 
+    private int[] NumbersLessThan(int[] nums)
+    {
+        // generar output
+        return nums;
+    }
+}
