b06b35b7b76d8d667e50a896ab0747fd0ef7e2ba
Tito STR Macbook pro <humberto.trujillo@uabc.edu.mx>
2021-02-25 19:12:51 -0800

    Practica 3

---
+++ diseno_de_interacciones-2021-1/Assets/Interactable.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+    public bool isInsideZone = false;
+    public KeyCode interactionKey = KeyCode.P;
+    
+    /// <summary>
+    /// Update is called every frame, if the MonoBehaviour is enabled.
+    /// </summary>
+    public virtual void Update()
+    {
+        if (isInsideZone && Input.GetKeyDown(interactionKey))
+        {
+            Interact();
+        }
+    }
+
+    /// <summary>
+    /// OnTriggerEnter is called when the Collider other enters the trigger.
+    /// </summary>
+    /// <param name="other">The other Collider involved in this collision.</param>
+    void OnTriggerEnter(Collider other)
+    {
+        if (!other.CompareTag("Player"))
+        {
+            return;
+        }
+        Debug.Log("Entró en el área");
+        isInsideZone = true;
+    }
+
+    /// <summary>
+    /// OnTriggerExit is called when the Collider other has stopped touching the trigger.
+    /// </summary>
+    /// <param name="other">The other Collider involved in this collision.</param>
+    void OnTriggerExit(Collider other)
+    {
+        if (!other.CompareTag("Player"))
+        {
+            return;
+        }
+        Debug.Log("Salió en el área");
+        isInsideZone = false;
+    }
+
+    public virtual void Interact()
+    {
+
+    }    
+}
--- diseno_de_interacciones-2021-1/Assets/Pelota.cs
+++ diseno_de_interacciones-2021-1/Assets/Pelota.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Pelota : MonoBehaviour
+public class Pelota : Interactable
 {
     public Vector3 direction;
     public Vector3 torque;
@@ -10,26 +10,29 @@ public class Pelota : MonoBehaviour
     public float magnusScale = 10;
     private Rigidbody rb;
     private float intensity;
+
     void Start()
     {
-        Debug.Log("Hola crayola!");
+        // Debug.Log("Hola crayola!");
         rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
-    void Update()
+    public override void Update()
     {
+        base.Update();
         //muevete 1 unidad de distancia cada frame
         //transform.Translate(direction * Time.deltaTime * speed);
-        if (Input.GetKeyDown(KeyCode.P))
-        {
-            rb.AddForce(direction*speed, ForceMode.Force);
-            rb.AddTorque(torque);
-        }
         Vector3 velocity = rb.velocity;
         Vector3 angularVelocity = rb.angularVelocity;
         intensity = 2 * Mathf.PI * magnusScale;
         Vector3 magnus = Vector3.Cross(velocity, angularVelocity * intensity);
         rb.AddForce(magnus);
+    }
+
+    public override void Interact()
+    {
+        rb.AddForce(direction*speed, ForceMode.Force);
+        rb.AddTorque(torque);
     }
 }
