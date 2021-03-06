251c183b3bde54a41d3ac53096292ac9ca7ae98e
Tito STR Macbook pro <humberto.trujillo@uabc.edu.mx>
2021-03-05 19:30:12 -0800

    Practica 4 Inventory

--- diseno_de_interacciones-2021-1/Assets/Inventory.cs
+++ diseno_de_interacciones-2021-1/Assets/Inventory.cs
@@ -1,11 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class Inventory : MonoBehaviour
 {
     static protected Inventory s_InventoryInstance;
     static public Inventory InventoryInstance { get { return s_InventoryInstance; } }
+
+    public delegate void OnChange();
+    public OnChange onChange;
+
     public int space = 10;
     public List<Item> items = new List<Item>();
 
@@ -14,11 +19,24 @@ public class Inventory : MonoBehaviour
         s_InventoryInstance = this;
     }
 
+    public Item[] GetAllItemsByType(ItemType type)
+    {
+        return items.Where(i => i.itemType == type).ToArray();
+    }
+
     public void Add(Item newItem)
     {
         if (items.Count < space)
         {
+            // if (newItem.GetType() == typeof(Pelota))
+            // {
+
+            // }
             items.Add(newItem);
+            if (onChange != null)
+            {
+                onChange.Invoke();
+            }
         }
         else
         {
@@ -31,6 +49,10 @@ public class Inventory : MonoBehaviour
         if (items.Contains(itemToRemove))
         {
             items.Remove(itemToRemove);
+            if (onChange != null)
+            {
+                onChange.Invoke();
+            }
         }
         else
         {
---
+++ diseno_de_interacciones-2021-1/Assets/InventoryUI.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryUI : MonoBehaviour
+{
+    private Inventory _inventory;
+    public GameObject panel;
+    void Start()
+    {
+        _inventory = Inventory.InventoryInstance;
+        _inventory.onChange += UpdateUI;
+    }
+
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            panel.SetActive(!panel.activeSelf);
+            UpdateUI();
+        }
+    }
+
+    void UpdateUI()
+    {
+        Slot[] slots = GetComponentsInChildren<Slot>();
+        Item[] equipItems = _inventory.GetAllItemsByType(ItemType.Equip);
+        Item[] medicineItems = _inventory.GetAllItemsByType(ItemType.Medicine);
+
+        if (equipItems.Length > 0)
+            slots[0].SetItem(equipItems[0], equipItems.Length);
+        if (medicineItems.Length > 0)
+            slots[1].SetItem(medicineItems[0], medicineItems.Length);
+        // for (int i = 0; i < slots.Length; i++)
+        // {
+            // if (i < _inventory.items.Count)
+            // {
+            //     slots[i].SetItem(_inventory.items[i]);
+            // }
+            // else
+            // {
+            //     slots[i].Clear();
+            // }
+        // }
+    }
+}
