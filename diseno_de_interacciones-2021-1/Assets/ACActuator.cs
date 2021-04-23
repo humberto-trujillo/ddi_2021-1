db0c22b613726599cfa8dfac933a94d7bbdca4a4
Tito STR Macbook pro <humberto.trujillo@uabc.edu.mx>
2021-04-22 19:48:29 -0700

    mqtt client

---
+++ diseno_de_interacciones-2021-1/Assets/ACActuator.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+
+using System;
+
+public class ACActuator : MonoBehaviour
+{
+    public string brokerEndpoint = "test.mosquitto.org";
+	public int brokerPort = 1883;
+	public string temperatureTopic = "casa/sala/temperatura";
+    public float temperatureUpperThreshold = 30f;
+    public float temperatureLowerThreshold = 20f;
+    private MqttClient client;
+    string lastMessage;
+    public GameObject acObject;
+    volatile bool acState = false;
+
+    void Start ()
+    {
+        client = new MqttClient(brokerEndpoint, brokerPort, false, null);
+        client.MqttMsgPublishReceived += client_MqttMsgPublishReceived;
+		string clientId = Guid.NewGuid().ToString(); 
+		client.Connect(clientId); 
+        client.Subscribe(new string[] { temperatureTopic }, new byte[] { MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE });
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (acState != acObject.activeSelf)
+			acObject.SetActive(acState);
+    }
+
+    void client_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e) 
+	{ 
+		Debug.Log("Received: " + System.Text.Encoding.UTF8.GetString(e.Message));
+		lastMessage = System.Text.Encoding.UTF8.GetString(e.Message);
+		float temp;
+        float.TryParse(lastMessage, out temp);
+        if ( temp >= temperatureUpperThreshold)
+        {
+            // acObject.SetActive(true);
+            acState = true;
+        }
+        else if (temp <= temperatureLowerThreshold)
+        {
+            // acObject.SetActive(false);
+            acState = false;
+        }
+	}
+}
---
+++ diseno_de_interacciones-2021-1/Assets/TemperatureSensor.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Net;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+
+using System;
+
+public class TemperatureSensor : MonoBehaviour
+{
+    public string brokerEndpoint = "test.mosquitto.org";
+	public int brokerPort = 1883;
+	public string temperatureTopic = "casa/sala/temperatura";
+    public float reportRate = 5f;
+    public float temperatureValue = 20.3f;
+    private MqttClient client;
+    private float reportTimer;
+
+    void Start ()
+    {
+		// create client instance 
+		//client = new MqttClient(IPAddress.Parse(brokerIpAddress), brokerPort, false, null);
+        client = new MqttClient(brokerEndpoint, brokerPort, false, null);
+		string clientId = Guid.NewGuid().ToString(); 
+		client.Connect(clientId); 
+	}
+
+    void Update()
+    {
+        if (!client.IsConnected)
+        {
+            Debug.LogWarning("No conectado");
+            return;
+        }
+        if ((reportTimer += Time.deltaTime) >= reportRate)
+        {
+            Debug.Log($"sending to topic {temperatureTopic}, value: {temperatureValue}");
+            string message = temperatureValue.ToString();
+			client.Publish(temperatureTopic, System.Text.Encoding.UTF8.GetBytes(message), MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, true);
+			Debug.Log("sent");
+            reportTimer = 0f;
+        }
+    }
+}
